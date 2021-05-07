438b4138f07711dac958708cc4e0b7e210b73ad1
jinyul80 <jinyul80@gmail.com>
2021-05-08 00:17:47 +0900

    동명이인 처리를 위해 수정

--- array_test/src/com/study/Main.java
+++ array_test/src/com/study/Main.java
@@ -1,13 +1,15 @@
 package com.study;
 
-import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] participants = {"Joe", "Leo", "James"};
-        String[] finished_person = {"Joe", "James"};
+        String[] participants = {"Joe", "Leo", "James", "Joe"};
+        String[] finished_person = {"Joe", "James", "Leo"};
 
         String fail_person = array_check1(participants, finished_person);
 
@@ -15,17 +17,53 @@ public class Main {
 
     }
 
-    public static String array_check1(String[] participants, String[] finished_list) {
-        String result = "";
+    public static String array_check1(String[] participant, String[] completion) {
+        String answer = "";
 
-        for (String person : participants) {
-            if (Arrays.stream(finished_list).noneMatch(person::equals)) {
-                result = person;
-                break;
+        Hashtable<String, Integer> part_ht = new Hashtable<>();
+        Hashtable<String, Integer> com_ht = new Hashtable<>();
+
+        for (String person : participant) {
+            if (part_ht.containsKey(person)) {
+                int cnt = part_ht.get(person) + 1;
+                part_ht.replace(person, cnt);
+
+            } else {
+                part_ht.put(person, 1);
             }
         }
 
-        return result;
+        for (String person : completion) {
+            if (com_ht.containsKey(person)) {
+                int cnt = com_ht.get(person) + 1;
+                com_ht.replace(person, cnt);
+
+            } else {
+                com_ht.put(person, 1);
+            }
+        }
+
+        Set keys = part_ht.keySet();
+
+        Iterator<String> iter = keys.iterator();
+
+        while (iter.hasNext()){
+            String key = iter.next();
+
+            if (com_ht.containsKey(key)) {
+                int part_cnt = part_ht.get(key);
+                int com_cnt = com_ht.get(key);
+
+                if ( part_cnt != com_cnt ) {
+                    answer = key;
+                }
+
+            } else {
+                answer = key;
+            }
+        }
+
+        return answer;
     }
 
 }
