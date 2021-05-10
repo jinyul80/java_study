17cbf3e192f5812fecb57630f9594539ed10de00
jinyul80 <jinyul80@gmail.com>
2021-05-10 17:42:15 +0900

    Hash문제1
    풀이 완료

--- array_test/src/com/study/Main.java
+++ array_test/src/com/study/Main.java
@@ -5,62 +5,43 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        Solution s1 = new Solution();
 
-//        String[] participants = {"Joe", "Leo", "James", "Joe"};
-//        String[] finished_person = {"Joe", "James", "Leo"};
-//
-//        String fail_person = array_check1(participants, finished_person);
-//
-//        System.out.println("탈락자: " + fail_person);
+        // Hash 테스트1
+        String[] participants = {"Joe", "Leo", "James", "Joe"};
+        String[] completion = {"Joe", "James", "Leo"};
 
-//        String[] phone_book = {"97674223", "1195524421", "119"};
+        String r1 = s1.solution1(participants, completion);
+
+        if (r1.equals("")) {
+            System.out.println("모두 완주성공");
+
+        } else {
+            System.out.println("탈락자: " + r1);
+        }
+
+        // Hash 테스트2
+        // String[] phone_book = {"97674223", "1195524421", "119"};
         String[] phone_book = {"123", "456", "789"};
 
-        Solution s1 = new Solution();
         boolean r = s1.solution2(phone_book);
-
-        System.out.println("Anser: " + r);
+        System.out.println("Answer: " + r);
 
     }
 
-    public static String array_check1(String[] participant, String[] completion) {
+}
+
+class Solution {
+
+    public String solution1(String[] participant, String[] completion) {
         String answer = "";
 
-        Hashtable<String, Integer> part_ht = new Hashtable<>();
-        Hashtable<String, Integer> com_ht = new Hashtable<>();
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String person : participant) hm.put(person, hm.getOrDefault(person, 0) + 1);
+        for (String person : completion) hm.put(person, hm.get(person) - 1);
 
-        for (String person : participant) {
-            if (part_ht.containsKey(person)) {
-                int cnt = part_ht.get(person) + 1;
-                part_ht.replace(person, cnt);
-
-            } else {
-                part_ht.put(person, 1);
-            }
-        }
-
-        for (String person : completion) {
-            if (com_ht.containsKey(person)) {
-                int cnt = com_ht.get(person) + 1;
-                com_ht.replace(person, cnt);
-
-            } else {
-                com_ht.put(person, 1);
-            }
-        }
-
-        Set<String> keys = part_ht.keySet();
-
-        for (String key : keys) {
-            if (com_ht.containsKey(key)) {
-                int part_cnt = part_ht.get(key);
-                int com_cnt = com_ht.get(key);
-
-                if (part_cnt != com_cnt) {
-                    answer = key;
-                }
-
-            } else {
+        for (String key : hm.keySet()){
+            if (hm.get(key) != 0) {
                 answer = key;
             }
         }
@@ -68,10 +49,6 @@ public class Main {
         return answer;
     }
 
-
-}
-
-class Solution {
     public boolean solution2(String[] phone_book) {
         boolean answer = true;
 
