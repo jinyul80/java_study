a5715f93d956855f2dc723c882635deeb8c08378
jinyul80 <jinyul80@gmail.com>
2021-05-10 10:16:51 +0900

    Hash문제2
    풀이 완료 - 효율성 통과를 위해 HashMap에 containsKey 사용

--- array_test/src/com/study/Main.java
+++ array_test/src/com/study/Main.java
@@ -13,7 +13,8 @@ public class Main {
 //
 //        System.out.println("탈락자: " + fail_person);
 
-        String[] phone_book = {"119", "97674223", "1195524421"};
+//        String[] phone_book = {"97674223", "1195524421", "119"};
+        String[] phone_book = {"123", "456", "789"};
 
         Solution s1 = new Solution();
         boolean r = s1.solution2(phone_book);
@@ -48,18 +49,14 @@ public class Main {
             }
         }
 
-        Set keys = part_ht.keySet();
+        Set<String> keys = part_ht.keySet();
 
-        Iterator<String> iter = keys.iterator();
-
-        while (iter.hasNext()){
-            String key = iter.next();
-
+        for (String key : keys) {
             if (com_ht.containsKey(key)) {
                 int part_cnt = part_ht.get(key);
                 int com_cnt = com_ht.get(key);
 
-                if ( part_cnt != com_cnt ) {
+                if (part_cnt != com_cnt) {
                     answer = key;
                 }
 
@@ -79,15 +76,18 @@ class Solution {
         boolean answer = true;
 
         HashMap<String, Integer> hm = new HashMap<>();
-        for (String pNumber : phone_book) {
-            hm.put(pNumber, 0);
-        }
+        for(String key : phone_book) hm.put(key, 0);
 
-        Set<String> keys = hm.keySet();
+        for (String sourceString : phone_book) {
+            for (int j = sourceString.length() - 1; j > 0; j--) {
+                String findString = sourceString.substring(0, j);
+                if (hm.containsKey(findString)) {
+                    answer = false;
+                    break;
+                }
+            }
 
-        for (String key : keys){
-            if(keys.stream().filter(s -> s.startsWith(key)).count() >= 2) {
-                answer = false;
+            if (!answer) {
                 break;
             }
         }
