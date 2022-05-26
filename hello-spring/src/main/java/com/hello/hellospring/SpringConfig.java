b48599a50da2bf591400c3610bca998c1b33c4cc
jinyul80 <jinyul80@gmail.com>
2022-05-27 00:21:19 +0900

    Hello Spring 프로젝트 추가

---
+++ hello-spring/src/main/java/com/hello/hellospring/SpringConfig.java
@@ -0,0 +1,47 @@
+package com.hello.hellospring;
+
+import com.hello.hellospring.aop.TimeTraceAop;
+import com.hello.hellospring.repository.MemberRepository;
+import com.hello.hellospring.service.MemberService;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class SpringConfig {
+
+    // private final DataSource dataSource;
+    //
+    // public SpringConfig(DataSource dataSource) {
+    //     this.dataSource = dataSource;
+    // }
+
+    // private final EntityManager em;
+    //
+    // @Autowired
+    // public SpringConfig(EntityManager em) {
+    //     this.em = em;
+    // }
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+    // @Bean
+    // public MemberRepository memberRepository() {
+    //     // return new MemoryMemberRepository();
+    //     // return new JdbcMemberRepository(dataSource);
+    //     return new JpaMemberRepository(em);
+    // }
+}
