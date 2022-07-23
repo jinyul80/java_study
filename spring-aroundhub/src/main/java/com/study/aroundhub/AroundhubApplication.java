ba9bc25ad072dc6437eec1b74ecdd2624a0f3fe8
jinyul80 <jinyul80@gmail.com>
2022-07-24 01:21:54 +0900

    spring-aroundhub 프로젝트 추가

---
+++ spring-aroundhub/src/main/java/com/study/aroundhub/AroundhubApplication.java
@@ -0,0 +1,13 @@
+package com.study.aroundhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroundhubApplication.class, args);
+	}
+
+}
---
+++ spring-aroundhub/src/main/java/com/study/aroundhub/dto/MemberDTO.java
@@ -0,0 +1,10 @@
+package com.study.aroundhub.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+    private String name;
+    private String email;
+    private String organization;
+}
