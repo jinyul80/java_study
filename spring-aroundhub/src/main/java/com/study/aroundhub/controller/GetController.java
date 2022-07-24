bc966856d567adfe5a06c9d8c599c206a15e74dd
jinyul80 <jinyul80@gmail.com>
2022-07-24 22:45:53 +0900

    spring-aroundhub 프로젝트
    GetController에 ulr 주석 추가

--- spring-aroundhub/src/main/java/com/study/aroundhub/controller/GetController.java
+++ spring-aroundhub/src/main/java/com/study/aroundhub/controller/GetController.java
@@ -9,21 +9,25 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    //url : http://localhost:8080/api/v1/get-api/name
     @GetMapping("/name")
     public String getName() {
         return "Jini";
     }
 
+    //url : http://localhost:8080/api/v1/get-api/variable1/testname
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
         return variable;
     }
 
+    //url : http://localhost:8080/api/v1/get-api/variable2/testname
     @GetMapping(value = "/variable2/{variable}")
     public String getVariable2(@PathVariable("variable")  String var) {
         return var;
     }
 
+    //url : http://localhost:8080/api/v1/get-api/request1?email=jini@gmail.com&name=jini&organization=Home
     @GetMapping(value = "/request1")
     public String getRequestParam1(
             @RequestParam String name,
@@ -33,6 +37,8 @@ public class GetController {
         return name + " " + email + " " + organization;
     }
 
+
+    //url : http://localhost:8080/api/v1/get-api/request2?email=jini@gmail.com&name=jini&organization=Home
     @GetMapping(value = "/request2")
     public String getRequestParam2(@RequestParam Map<String, String> param) {
         StringBuilder sb = new StringBuilder();
@@ -44,6 +50,7 @@ public class GetController {
         return sb.toString();
     }
 
+    //url : http://localhost:8080/api/v1/get-api/request3?email=jini@gmail.com&name=jini&organization=Home
     @GetMapping(value = "/request3")
     public String getRequestParam3(MemberDTO memberDTO) {
         return memberDTO.toString();
