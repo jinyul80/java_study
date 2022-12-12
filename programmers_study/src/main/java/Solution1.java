6d2c0b666fe14af512e16126fe0251b136236b28
Jinyeol Kim <jinyul80@gmail.com>
2022-12-12 13:00:19 +0900

    Programmers study
    스택/큐 - 프린터 문제 추가

--- programmers_study/src/main/java/Solution1.java
+++ programmers_study/src/main/java/Solution1.java
@@ -1,6 +1,36 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution1 {
     public void printHelloWorld() {
         System.out.println("Hello World!!!");
+    }
+
+    public int solution1(int[] priorities, int location) {
+        int answer = 0;
+
+        // [idx, priority]
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int idx = 0; idx < priorities.length ; idx++) {
+            int[] task = {idx, priorities[idx]};
+            queue.add(task);
+        }
+
+        while (!queue.isEmpty()){
+            int[] task = queue.remove();
+            if (queue.stream().anyMatch(x -> x[1] > task[1])) {
+                queue.add(task);
+            } else {
+                answer++;
+                if (task[0] == location) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
     }
 
 }
--- programmers_study/src/test/java/Solution1Test.java
+++ programmers_study/src/test/java/Solution1Test.java
@@ -9,4 +9,22 @@ class Solution1Test {
     void printHelloWorld() {
         s1.printHelloWorld();
     }
+
+    @Test
+    void sol_프린터1(){
+        int[] priorities = {2, 1, 3, 2};
+        int locatino = 2;
+
+        int result = s1.solution1(priorities, locatino);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    void sol_프린터2(){
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int locatino = 0;
+
+        int result = s1.solution1(priorities, locatino);
+        assertEquals(result, 5);
+    }
 }
