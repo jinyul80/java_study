49f20c94236562c5e3292f434e18aab6e5da4b10
Jinyeol Kim <jinyul80@gmail.com>
2022-12-12 10:30:10 +0900

    Programmers study 프로젝트 추가

---
+++ programmers_study/src/main/java/Solution1.java
@@ -0,0 +1,6 @@
+public class Solution1 {
+    public void printHelloWorld() {
+        System.out.println("Hello World!!!");
+    }
+
+}
---
+++ programmers_study/src/test/java/Solution1Test.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution1Test {
+
+    Solution1 s1 = new Solution1();
+    @Test
+    void printHelloWorld() {
+        s1.printHelloWorld();
+    }
+}
