06c298d2615e81b4dcbf36c40e1705b41b9bce7f
Jinyeol Kim <jinyul80@gmail.com>
2022-12-23 13:55:41 +0900

    Programmers study
    약수의 개수 구하기 성능 비교

--- programmers_study/src/main/java/Solution1.java
+++ programmers_study/src/main/java/Solution1.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class Solution1 {
@@ -32,5 +34,46 @@ public class Solution1 {
 
         return answer;
     }
+
+    // 약수의 개수 구하기
+    public int getDivisorCount1(long number) {
+        int divisorCount = 0;
+        for(int i = 1; i <= Math.sqrt(number); i++) {
+            if (i == Math.sqrt(number)) {
+                divisorCount++;
+            } else if (number % i == 0) {
+                divisorCount += 2;
+            }
+        }
+
+        return divisorCount;
+    }
+
+    public int getDivisorCount2(long number) {
+        int divisorCount = 0;
+
+        Map<Long, Integer> map = new HashMap<>();
+        int sqrtNum = (int) Math.sqrt(number);
+
+        for (long i = 2; i <= sqrtNum; i++) {
+            while (number % i == 0) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
+                number /= i;
+            }
+        }
+
+        if (number > 1) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+
+        divisorCount = 1;
+        for (int cnt : map.values()) {
+            divisorCount *= cnt + 1;
+        }
+
+        return divisorCount;
+    }
+
+
 
 }
--- programmers_study/src/test/java/Solution1Test.java
+++ programmers_study/src/test/java/Solution1Test.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class Solution1Test {
@@ -26,5 +29,33 @@ class Solution1Test {
 
         int result = s1.solution1(priorities, locatino);
         assertEquals(result, 5);
+    }
+
+    @Test
+    void sol_약수개수성능비교(){
+
+        long number = 1567349871579687498L;
+        
+        Map<Integer, Integer> sol1map = new HashMap<>();
+        Map<Integer, Integer> sol2map = new HashMap<>();
+
+        // Loop 타입
+        long beforeTime = System.currentTimeMillis();
+        int count1 = s1.getDivisorCount1(number);
+        long afterTime = System.currentTimeMillis();
+        long secDiffTime = (afterTime - beforeTime);
+        NumberFormat formater = NumberFormat.getInstance();
+        
+        System.out.printf("Loop 방식: %s (ms)\n", formater.format(secDiffTime));
+
+        // 소인수분해 타입
+        beforeTime = System.currentTimeMillis();
+        int count2 = s1.getDivisorCount2(number);
+        afterTime = System.currentTimeMillis();
+        secDiffTime = (afterTime - beforeTime);
+        System.out.printf("소인수분해 방식: %s (ms)\n", formater.format(secDiffTime));
+
+        System.out.printf("Number: %d, count1: %d, count2: %d\n", number, count1, count2);
+
     }
 }
